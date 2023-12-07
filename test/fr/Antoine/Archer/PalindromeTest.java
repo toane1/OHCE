4ea1e1ecfcd480c0b3714548a85d51135877cb2d
antoine <archer.antoine@gmail.com>
2023-12-07 12:36:32 +0100

    test palindrome bjr et aurevoir

--- test/fr/Antoine/Archer/PalindromeTest.java
+++ test/fr/Antoine/Archer/PalindromeTest.java
@@ -1,10 +1,7 @@
 package fr.Antoine.Archer;
 
 import fr.Antoine.Archer.TestBuilders.VerificationPalindromeBuilder;
-import fr.AntoineArcher.Expressions;
-import fr.AntoineArcher.Langue;
-import fr.AntoineArcher.LangueFrancais;
-import fr.AntoineArcher.VerificationPalindrome;
+import fr.AntoineArcher.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -31,40 +28,105 @@ public class PalindromeTest {
         String palindrome = "radar";
         // et un utilisateur en langue française
         Langue langue = new LangueFrancais();
-        VerificationPalindrome VerificationPalindrome = new VerificationPalindromeBuilder()
+        VerificationPalindrome verificationPalindrome = new VerificationPalindromeBuilder()
                 .AyantPourLangue(langue)
                 .Build();
         // QUAND on vérifie si c'est un palindrome
-        String resultat = VerificationPalindrome.Verifier(palindrome);
+        String resultat = verificationPalindrome.Verifier(palindrome);
 
         // ALORS la chaîne est répétée, suivie de "Bien dit !"
         String attendu = palindrome + System.lineSeparator() + Expressions.BIENDIT;
         assertTrue(resultat.contains(attendu));
     }
+    @Test
+    public void testPalindromeAnglais(){
+        // ETANT DONNE un palindrome
+        String palindrome = "radar";
+        // et un utilisateur en langue anglaise
+        Langue langue = new LangueAnglais();
+        VerificationPalindrome verificationPalindrome = new VerificationPalindromeBuilder()
+                .AyantPourLangue(langue)
+                .Build();
+        // QUAND on vérifie si c'est un palindrome
+        String resultat = verificationPalindrome.Verifier(palindrome);
+
+        // ALORS la chaîne est répétée, suivie de "Well Done !"
+        String attendu = palindrome + System.lineSeparator() + Expressions.WELLDONE;
+        assertTrue(resultat.contains(attendu));
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"test", "radar"})
-    public void testBonjour(String chaine){
+    public void testBonjourFrancais(String chaine){
         // ETANT DONNE une chaîne
+        //
+        // et un utilisateur en langue française
+                Langue langue = new LangueFrancais();
+                VerificationPalindrome verificationPalindrome = new VerificationPalindromeBuilder()
+                        .AyantPourLangue(langue)
+                        .Build();
         // QUAND on vérifie si c'est un palindrome
-        String resultat = VerificationPalindromeBuilder.Default().Verifier(chaine);
+        // QUAND on vérifie si c'est un palindrome
+        String resultat = verificationPalindrome.Verifier(chaine);
 
         // ALORS toute réponse est précédée de "Bonjour"
         String[] lines = resultat.split(System.lineSeparator());
-        assertEquals(Expressions.BONJOUR, lines[0]);
+        String firstLine = lines[0];
+        assertEquals(Expressions.BONJOUR, firstLine);
     }
     @ParameterizedTest
     @ValueSource(strings = {"test", "radar"})
-    public void testAuRevoir(String chaine){
+    public void testBonjourAnglais(String chaine){
         // ETANT DONNE une chaîne
+        //
+        // et un utilisateur en langue française
+        Langue langue = new LangueAnglais();
+        VerificationPalindrome verificationPalindrome = new VerificationPalindromeBuilder()
+                .AyantPourLangue(langue)
+                .Build();
         // QUAND on vérifie si c'est un palindrome
-        String résultat = VerificationPalindromeBuilder.Default().Verifier(chaine);
+        // QUAND on vérifie si c'est un palindrome
+        String resultat = verificationPalindrome.Verifier(chaine);
+
+        // ALORS toute réponse est précédée de "Bonjour"
+        String[] lines = resultat.split(System.lineSeparator());
+        String firstLine = lines[0];
+        assertEquals(Expressions.HELLO, firstLine);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test", "radar"})
+    public void testAuRevoirFrancais(String chaine){
+        // ETANT DONNE une chaîne
+        // et un utilisateur en langue française
+        Langue langue = new LangueFrancais();
+        VerificationPalindrome verificationPalindrome = new VerificationPalindromeBuilder()
+                .AyantPourLangue(langue)
+                .Build();
+        // QUAND on vérifie si c'est un palindrome
+        String resultat = verificationPalindrome.Verifier(chaine);
 
         // ALORS toute réponse est suivie de "Au Revoir"
-        String[] lines = résultat.split(System.lineSeparator());
+        String[] lines = resultat.split(System.lineSeparator());
         String lastLine = lines[lines.length - 1];
         assertEquals(Expressions.AUREVOIR, lastLine);
     }
 
+    @ParameterizedTest
+    @ValueSource(strings =  {"test", "radar"})
+    public void testAuRevoirAnglais(String chaine){
+        // ETANT DONNE une chaîne
+        // et un utilisateur en langue française
+        Langue langue = new LangueAnglais();
+        VerificationPalindrome verificationPalindrome = new VerificationPalindromeBuilder()
+                .AyantPourLangue(langue)
+                .Build();
+        // QUAND on vérifie si c'est un palindrome
+        String resultat = verificationPalindrome.Verifier(chaine);
+        // ALORS toute réponse est suivie de "Au Revoir"
+        String[] lines = resultat.split(System.lineSeparator());
+        String lastline = lines[lines.length - 1];
+        assertEquals(Expressions.BYE, lastline);
+    }
 
 }
